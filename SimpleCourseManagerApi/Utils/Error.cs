c15f98cdbc272216207e4484d1c265cfcdc2b766
mohammadsofan <mohammadsof72@gmail.com>
2025-08-04 13:02:54 +0300

    feat(utils): add Error helper class for structured error responses

---
+++ SimpleCourseManagerApi/Utils/Error.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleCourseManagerApi.Utils
+{
+    public class Error
+    {
+        public string Field { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+    }
+}

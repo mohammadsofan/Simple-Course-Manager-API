87c8ec9a59d8c1d13c85bab87ffca68cf8d92617
mohammadsofan <mohammadsof72@gmail.com>
2025-08-04 13:04:19 +0300

    feat(validation): create custom validator for course input

---
+++ SimpleCourseManagerApi/Validators/CourseValidator.cs
@@ -0,0 +1,58 @@
+﻿using SimpleCourseManagerApi.Dtos.Request;
+using SimpleCourseManagerApi.Interfaces;
+using SimpleCourseManagerApi.Utils;
+
+namespace SimpleCourseManagerApi.Validators
+{
+    public class CourseValidatorResult
+    {
+        public bool IsValid {  get; set; }
+        public IList<Error> Errors { get; set; } = new List<Error>();
+    }
+
+    public class CourseValidator:IValidator<CourseValidatorResult,CourseRequest> { 
+        public CourseValidatorResult IsValid(CourseRequest course)
+        {
+            IList<Error> errors = new List<Error>();
+            if (course.Name is null || course.Name.Length < 3)
+            {
+                errors.Add(new Error
+                {
+                    Field = "Name",
+                    Message = "Name must be 3 characters at least"
+                });
+            }
+            if(course.Description is null || course.Description.Length < 3)
+            {
+                errors.Add(new Error
+                {
+                    Field = "Description",
+                    Message = "Description must be 3 characters at least"
+                });
+            }
+            if(course.Price < 0)
+            {
+                errors.Add(new Error
+                {
+                    Field = "Price",
+                    Message = "Price must be positive value"
+                });
+            }
+            if(course.Subject is null || course.Subject.Length < 2)
+            {
+                errors.Add(new Error
+                {
+                    Field = "Subject",
+                    Message = "Subject must be 2 characters at least"
+                });
+            }
+
+            return new CourseValidatorResult()
+            {
+                IsValid = errors.Count == 0,
+                Errors = errors
+            };
+        }
+    }
+}
+ 
